c641ecd185c761c465537158a4ffe0f9a5b4b0d5
Sxmourai <samourai_x@outlook.fr>
2024-03-16 21:29:59 +0100

    feature: creating img instead of bevy Bevy doesn't support pixel writing without shaders afaik, so using an img, and will change it later !

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,51 @@
 #![feature(optimize_attribute)]
 
-use bevy::{prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}};
+use std::io::Write;
+
+pub const IMG_WIDTH: u32 = 1920;
+pub const IMG_HEIGHT: u32 = 1080;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .run();
+    let mut screen = vec![0u8; (IMG_WIDTH * IMG_HEIGHT) as usize * 3];
+    for row in 0..IMG_HEIGHT {
+        for column in 0..IMG_WIDTH {
+            let ray = send_ray(row, column);
+            screen[((row*IMG_WIDTH+column)*3+0) as usize] = ray.r;
+            screen[((row*IMG_WIDTH+column)*3+1) as usize] = ray.g;
+            screen[((row*IMG_WIDTH+column)*3+2) as usize] = ray.b;
+        }
+        print!("Line: {}/{IMG_HEIGHT}...\r", row+1);
+        std::io::stdout().flush().unwrap();
+    }
+    save(&screen)
 }
 
+struct Ray {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+fn send_ray(row: u32, column: u32) -> Ray {
+    let mut r = Ray {
+        r: 0,
+        g: 0,
+        b: 0,
+    };
+    
+    r
+}
+
+
 #[optimize(speed)]
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Camera2dBundle::default());
+fn save(data: &[u8]) {
+    let path = std::path::Path::new(r"raycasted.png");
+    let file = std::fs::File::create(path).unwrap();
 
-    let shapes = [
-        Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-        Mesh2dHandle(meshes.add(Triangle2d::new(
-            Vec2::Y * 50.0,
-            Vec2::new(-50.0, -50.0),
-            Vec2::new(50.0, -50.0),
-        ))),
-    ];
-    let num_shapes = shapes.len();
+    let mut encoder = png::Encoder::new(file, IMG_WIDTH, IMG_HEIGHT);
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
 
-    for (i, shape) in shapes.into_iter().enumerate() {
-        // Distribute colors evenly across the rainbow.
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
-
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: shape,
-            material: materials.add(color),
-            transform: Transform::from_xyz(
-                // Distribute shapes from -600. to +600..
-                -600. / 2. + i as f32 / (num_shapes - 1) as f32 * 600.,
-                0.0,
-                0.0,
-            ),
-            ..default()
-        });
-    }
+    writer.write_image_data(&data).unwrap(); // Save
 }
